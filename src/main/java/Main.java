4fb8497d452264fbcd783c998720cdc4467aa562
ddudalves <169068338+ddudalves@users.noreply.github.com>
2024-09-02 12:02:02 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.utilScanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = sc.nextInt(System.in);
+    int numero = sc.nextInt();
+        System.out.println("O número digitado foi:");
+    System.out.println(número);
+    if(numero > 0){
+      System.out.println(O número é positivo");
+    }
+      if(número < 0){
+        System.out.prinntln("O número é negativo");
+  }
+
+  if (numero == 0) {
+    System.out.println("O número é igual a zero:);
+
+  }
+sc.close();
+  
+}
